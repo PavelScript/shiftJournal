c337f48ec7ddee3b4ce4b190abc8757ae1921b36
Pavel <sir.buzmakoff@yandex.ru>
2025-05-29 00:14:12 +0500

    bug fix

--- src/components/AddModalZayavka/AddModalZayavka.jsx
+++ src/components/AddModalZayavka/AddModalZayavka.jsx
@@ -28,7 +28,7 @@ const AddModalZayavka = ({ onClose }) => {
       alert("Введите принявшего заявку");
     } else {
       try {
-        const response = await fetch("http://localhost:5000/api/add", {
+        const response = await fetch("http://46.173.29.202:5000/api/add", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(formData),
